080a75a140945825805b70011a14a0d824f9cbaf
ShotaTogawa <s.togawa0112@gmail.com>
2020-02-14 22:04:08 -0800

    initial setup for websocket

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # node_websocket
+
+- Websockets allow for full-duplex communication
+- WebSocket is a separate protocol from HTTP
+- Persistent connection between client and server
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -8,6 +13,10 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 // app.use(express.json());
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log("new websocket connection");
+});
+
+server.listen(port, () => {
   console.log(`listening ${port}`);
 });
